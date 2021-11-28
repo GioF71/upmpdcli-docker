5f1e0d8b282e6bc1d15c6456d283993b1373135a
GioF_71 <giovanni.fulco@gmail.com>
2021-11-28 11:59:18 +0100

    Text substitution is not executed once

--- run-upmpdcli.sh
+++ run-upmpdcli.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
-sed -i 's/UPMPD_FRIENDLY_NAME/'"$UPMPD_FRIENDLY_NAME"'/g' /etc/upmpdcli.conf
-sed -i 's/AV_FRIENDLY_NAME/'"$AV_FRIENDLY_NAME"'/g' /etc/upmpdcli.conf
+CONFIG_FILE=/etc/upmpdcli-config-file.conf
 
-sed -i 's/MPD_HOST/'"$MPD_HOST"'/g' /etc/upmpdcli.conf
-sed -i 's/MPD_PORT/'"$MPD_PORT"'/g' /etc/upmpdcli.conf
-
-echo "Tidal Enable: $TIDAL_ENABLE"
-if [ "$TIDAL_ENABLE" == "yes" ]; then \
-    echo "Processing Tidal settings"; \
-    sed -i 's/\#tidaluser/tidaluser/g' /etc/upmpdcli.conf; \
-    sed -i 's/\#tidalpass/tidalpass/g' /etc/upmpdcli.conf; \
-    sed -i 's/\#tidalapitoken/tidalapitoken/g' /etc/upmpdcli.conf; \
-    sed -i 's/\#tidalquality/tidalquality/g' /etc/upmpdcli.conf; \
-    sed -i 's/TIDAL_USERNAME/'"$TIDAL_USERNAME"'/g' /etc/upmpdcli.conf; \
-    sed -i 's/TIDAL_PASSWORD/'"$TIDAL_PASSWORD"'/g' /etc/upmpdcli.conf; \
-    sed -i 's/TIDAL_API_TOKEN/'"$TIDAL_API_TOKEN"'/g' /etc/upmpdcli.conf; \
-    sed -i 's/TIDAL_QUALITY/'"$TIDAL_QUALITY"'/g' /etc/upmpdcli.conf; \
+if test -f "$CONFIG_FILE"; then 
+    echo "Configuration file [$CONFIG_FILE] already exists, skipping text substitution"
+else 
+    echo "Configuration file [$CONFIG_FILE] not found, creating."
+    cp /etc/upmpdcli.conf $CONFIG_FILE
+    sed -i 's/UPMPD_FRIENDLY_NAME/'"$UPMPD_FRIENDLY_NAME"'/g' $CONFIG_FILE
+    sed -i 's/AV_FRIENDLY_NAME/'"$AV_FRIENDLY_NAME"'/g' $CONFIG_FILE
+    sed -i 's/MPD_HOST/'"$MPD_HOST"'/g' $CONFIG_FILE
+    sed -i 's/MPD_PORT/'"$MPD_PORT"'/g' $CONFIG_FILE
+    echo "Tidal Enable: $TIDAL_ENABLE"
+    if [ "$TIDAL_ENABLE" == "yes" ]; then \
+        echo "Processing Tidal settings"; \
+        sed -i 's/\#tidaluser/tidaluser/g' $CONFIG_FILE; \
+        sed -i 's/\#tidalpass/tidalpass/g' $CONFIG_FILE; \
+        sed -i 's/\#tidalapitoken/tidalapitoken/g' $CONFIG_FILE; \
+        sed -i 's/\#tidalquality/tidalquality/g' $CONFIG_FILE; \
+        sed -i 's/TIDAL_USERNAME/'"$TIDAL_USERNAME"'/g' $CONFIG_FILE; \
+        sed -i 's/TIDAL_PASSWORD/'"$TIDAL_PASSWORD"'/g' $CONFIG_FILE; \
+        sed -i 's/TIDAL_API_TOKEN/'"$TIDAL_API_TOKEN"'/g' $CONFIG_FILE; \
+        sed -i 's/TIDAL_QUALITY/'"$TIDAL_QUALITY"'/g' $CONFIG_FILE; \
+    fi
+    echo "Qobuz Enable: $QOBUZ_ENABLE"
+    if [ "$QOBUZ_ENABLE" == "yes" ]; then \
+        echo "Processing Qobuz settings"; \
+        sed -i 's/\#qobuzuser/qobuzuser/g' $CONFIG_FILE; \
+        sed -i 's/\#qobuzpass/qobuzpass/g' $CONFIG_FILE; \
+        sed -i 's/\#qobuzformatid/qobuzformatid/g' $CONFIG_FILE; \
+        sed -i 's/QOBUZ_USERNAME/'"$QOBUZ_USERNAME"'/g' $CONFIG_FILE; \
+        sed -i 's/QOBUZ_PASSWORD/'"$QOBUZ_PASSWORD"'/g' $CONFIG_FILE; \
+        sed -i 's/QOBUZ_FORMAT_ID/'"$QOBUZ_FORMAT_ID"'/g' $CONFIG_FILE; \
+    fi
+    cat $CONFIG_FILE
 fi
-
-echo "Qobuz Enable: $QOBUZ_ENABLE"
-if [ "$QOBUZ_ENABLE" == "yes" ]; then \
-    echo "Processing Qobuz settings"; \
-    sed -i 's/\#qobuzuser/qobuzuser/g' /etc/upmpdcli.conf; \
-    sed -i 's/\#qobuzpass/qobuzpass/g' /etc/upmpdcli.conf; \
-    sed -i 's/\#qobuzformatid/qobuzformatid/g' /etc/upmpdcli.conf; \
-    sed -i 's/QOBUZ_USERNAME/'"$QOBUZ_USERNAME"'/g' /etc/upmpdcli.conf; \
-    sed -i 's/QOBUZ_PASSWORD/'"$QOBUZ_PASSWORD"'/g' /etc/upmpdcli.conf; \
-    sed -i 's/QOBUZ_FORMAT_ID/'"$QOBUZ_FORMAT_ID"'/g' /etc/upmpdcli.conf; \
-fi
-
-cat /etc/upmpdcli.conf
 
 echo "About to sleep for $STARTUP_DELAY_SEC second(s)"
 sleep $STARTUP_DELAY_SEC
-echo "Rise and shine!"
+echo "Ready to start."
 
-/usr/bin/upmpdcli -c /etc/upmpdcli.conf
+/usr/bin/upmpdcli -c $CONFIG_FILE
+
