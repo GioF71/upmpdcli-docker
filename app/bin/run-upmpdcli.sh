cb2f48f2ad254fa036143b5fa902d40f1973f997
GioF_71 <giovanni.fulco@gmail.com>
2022-10-26 00:27:21 +0200

    removed commented code

--- app/bin/run-upmpdcli.sh
+++ app/bin/run-upmpdcli.sh
@@ -32,95 +32,14 @@ else
 fi
 
 replace_parameter $CONFIG_FILE UPNPPORT "$UPNPPORT" upnpport
-#echo "UPNPPORT=["$UPNPPORT"]"
-#if [ -z "${UPNPPORT}" ]; then
-#    echo "UPNPPORT not set"
-#else 
-#    echo "Setting UPNPPORT to ["$UPNPPORT"]"
-#    sed -i 's/\#upnpport/upnpport/g' $CONFIG_FILE;
-#    sed -i 's/UPNPPORT/'"$UPNPPORT"'/g' $CONFIG_FILE;
-#fi
-
 replace_parameter $CONFIG_FILE UPMPD_FRIENDLY_NAME "$UPMPD_FRIENDLY_NAME" friendlyname
-#echo "UPMPD_FRIENDLY_NAME=["$UPMPD_FRIENDLY_NAME"]"
-#if [ -z "${UPMPD_FRIENDLY_NAME}" ]; then
-#    echo "UPMPD_FRIENDLY_NAME not set"
-#else 
-#    echo "Setting UPMPD_FRIENDLY_NAME to ["$UPMPD_FRIENDLY_NAME"]"
-#    sed -i 's/\#friendlyname/friendlyname/g' $CONFIG_FILE;
-#    sed -i 's/UPMPD_FRIENDLY_NAME/'"$UPMPD_FRIENDLY_NAME"'/g' $CONFIG_FILE;
-#fi
-
 replace_parameter $CONFIG_FILE UPNPAV "$UPNPAV" upnpav
-#echo "UPNPAV=["$UPNPAV"]"
-#if [ -z "${UPNPAV}" ]; then
-#    echo "UPNPAV not set"
-#else 
-#    echo "Setting UPNPAV to ["$UPNPAV"]"
-#    sed -i 's/\#upnpav/upnpav/g' $CONFIG_FILE;
-#    sed -i 's/UPNPAV/'"$UPNPAV"'/g' $CONFIG_FILE;
-#fi
-
 replace_parameter $CONFIG_FILE OPENHOME "$OPENHOME" openhome
-#echo "OPENHOME=["$OPENHOME"]"
-#if [ -z "${OPENHOME}" ]; then
-#    echo "OPENHOME not set"
-#else 
-#    echo "Setting OPENHOME to ["$OPENHOME"]"
-#    sed -i 's/\#openhome/openhome/g' $CONFIG_FILE;
-#    sed -i 's/OPENHOME/'"$OPENHOME"'/g' $CONFIG_FILE;
-#fi
-
 replace_parameter $CONFIG_FILE AV_FRIENDLY_NAME "$AV_FRIENDLY_NAME" avfriendlyname
-#echo "AV_FRIENDLY_NAME=["$AV_FRIENDLY_NAME"]"
-#if [ -z "${AV_FRIENDLY_NAME}" ]; then
-#    echo "AV_FRIENDLY_NAME not set"
-#else 
-#    echo "Setting AV_FRIENDLY_NAME to ["$AV_FRIENDLY_NAME"]"
-#    sed -i 's/\#avfriendlyname/avfriendlyname/g' $CONFIG_FILE;
-#    sed -i 's/AV_FRIENDLY_NAME/'"$AV_FRIENDLY_NAME"'/g' $CONFIG_FILE;
-#fi
-
 replace_parameter $CONFIG_FILE MPD_HOST "$MPD_HOST" mpdhost
-#echo "MPD_HOST=["$MPD_HOST"]"
-#if [ -z "${MPD_HOST}" ]; then
-#    echo "MPD_HOST not set"
-#else 
-#    echo "Setting MPD_HOST to ["$MPD_HOST"]"
-#    sed -i 's/\#mpdhost/mpdhost/g' $CONFIG_FILE;
-#    sed -i 's/MPD_HOST/'"$MPD_HOST"'/g' $CONFIG_FILE;
-#fi
-
 replace_parameter $CONFIG_FILE MPD_PORT "$MPD_PORT" mpdport
-#echo "MPD_PORT=["$MPD_PORT"]"
-#if [ -z "${MPD_PORT}" ]; then
-#    echo "MPD_PORT not set"
-#else 
-#    echo "Setting MPD_PORT to ["$MPD_PORT"]"
-#    sed -i 's/\#mpdport/mpdport/g' $CONFIG_FILE;
-#    sed -i 's/MPD_PORT/'"$MPD_PORT"'/g' $CONFIG_FILE;
-#fi
-
 replace_parameter $CONFIG_FILE PLG_MICRO_HTTP_HOST "$PLG_MICRO_HTTP_HOST" plgmicrohttphost
-#echo "PLG_MICRO_HTTP_HOST=["$PLG_MICRO_HTTP_HOST"]"
-#if [ -z "${PLG_MICRO_HTTP_HOST}" ]; then
-#    echo "PLG_MICRO_HTTP_HOST not set"
-#else 
-#    echo "Setting PLG_MICRO_HTTP_HOST to ["$PLG_MICRO_HTTP_HOST"]"
-#    sed -i 's/\#plgmicrohttphost/plgmicrohttphost/g' $CONFIG_FILE;
-#    sed -i 's/PLG_MICRO_HTTP_HOST/'"$PLG_MICRO_HTTP_HOST"'/g' $CONFIG_FILE;
-#fi
-
 replace_parameter $CONFIG_FILE PLG_MICRO_HTTP_PORT "$PLG_MICRO_HTTP_PORT" plgmicrohttpport
-#echo "PLG_MICRO_HTTP_PORT=["$PLG_MICRO_HTTP_PORT"]"
-#if [ -z "${PLG_MICRO_HTTP_PORT}" ]; then
-#    echo "PLG_MICRO_HTTP_PORT not set"
-#else 
-#    echo "Setting PLG_MICRO_HTTP_PORT to ["$PLG_MICRO_HTTP_PORT"]"
-#    sed -i 's/\#plgmicrohttpport/plgmicrohttpport/g' $CONFIG_FILE;
-#    sed -i 's/PLG_MICRO_HTTP_PORT/'"$PLG_MICRO_HTTP_PORT"'/g' $CONFIG_FILE;
-#fi
-
 replace_parameter $CONFIG_FILE MEDIA_SERVER_FRIENDLY_NAME "$MEDIA_SERVER_FRIENDLY_NAME" msfriendlyname
 
 echo "Tidal Enable [$TIDAL_ENABLE]"
@@ -135,6 +54,7 @@ if [ "$TIDAL_ENABLE" == "yes" ]; then
     sed -i 's/TIDAL_API_TOKEN/'"$TIDAL_API_TOKEN"'/g' $CONFIG_FILE;
     sed -i 's/TIDAL_QUALITY/'"$TIDAL_QUALITY"'/g' $CONFIG_FILE;
 fi
+
 echo "Qobuz Enable [$QOBUZ_ENABLE]"
 if [ "$QOBUZ_ENABLE" == "yes" ]; then
     echo "Processing Qobuz settings";
