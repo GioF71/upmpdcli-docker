a8c1316b4ba237df260a54b99f18e7abd45fe86b
Giovanni Fulco <giovanni.fulco@gmail.com>
2023-08-18 19:12:57 +0200

    renderer mode fix

--- app/install/setup-ubuntu.sh
+++ app/install/setup-ubuntu.sh
@@ -10,33 +10,31 @@ set -ex
 UBUNTU_VERSION=$(echo $BASE_IMAGE | cut -d ":" -f 2)
 echo "UBUNTU_VERSION=[$UBUNTU_VERSION]"
 
-declare -A needs_switch
-needs_switch[jammy]=0
-needs_switch[lunar]=1
-
-python_packages=(pyradios py-sonic subsonic-connector==0.1.17 mutagen)
-
-add_switch=${needs_switch[$UBUNTU_VERSION]}
-
-#upgrade pip
-pip_upgrade="pip install --upgrade pip"
-if [ $add_switch -eq 1 ]; then
-    pip_upgrade="$pip_upgrade --break-system-packages"
-fi
-echo "pip_upgrade=[$pip_upgrade]"
-eval "$pip_upgrade"
-
-for pkg in "${python_packages[@]}"
-do
-    echo "Installing ${pkg} with add_switch [$add_switch]..."
-    cmd="pip install "
+if [ "$BUILD_MODE" = "full" ]; then
+    declare -A needs_switch
+    needs_switch[jammy]=0
+    needs_switch[lunar]=1
+    python_packages=(pyradios py-sonic subsonic-connector==0.1.17 mutagen)
+    add_switch=${needs_switch[$UBUNTU_VERSION]}
+    #upgrade pip
+    pip_upgrade="pip install --upgrade pip"
     if [ $add_switch -eq 1 ]; then
-        cmd="$cmd --break-system-packages"
+        pip_upgrade="$pip_upgrade --break-system-packages"
     fi
-    cmd="$cmd ${pkg}"
-    echo "cmd=[$cmd]"
-    eval "$cmd"
-done
+    echo "pip_upgrade=[$pip_upgrade]"
+    eval "$pip_upgrade"
+    for pkg in "${python_packages[@]}"
+    do
+        echo "Installing ${pkg} with add_switch [$add_switch]..."
+        cmd="pip install "
+        if [ $add_switch -eq 1 ]; then
+            cmd="$cmd --break-system-packages"
+        fi
+        cmd="$cmd ${pkg}"
+        echo "cmd=[$cmd]"
+        eval "$cmd"
+    done
+fi
 
 apt-get update
 apt-get install -y software-properties-common
