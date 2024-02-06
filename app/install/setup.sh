87cf80395d2df4cec0fc78a004836960ec6bb44b
Giovanni Fulco <giovanni.fulco@gmail.com>
2024-02-06 19:46:26 +0100

    Subsonic: advance subsonic-connector to 0.3.2

--- app/install/setup.sh
+++ app/install/setup.sh
@@ -27,7 +27,7 @@ if [ "$BUILD_MODE" = "full" ]; then
     if [ $add_switch -eq 1 ]; then
         pip_upgrade="$pip_upgrade --break-system-packages"
     fi
-    python_packages=(pyradios py-sonic subsonic-connector==0.2.6 mutagen rdflib mpegdash tidalapi==0.7.6)
+    python_packages=(pyradios py-sonic subsonic-connector==0.3.2 mutagen rdflib mpegdash tidalapi==0.7.6)
     for pkg in "${python_packages[@]}"
     do
         echo "Installing ${pkg} with add_switch [$add_switch]..."
