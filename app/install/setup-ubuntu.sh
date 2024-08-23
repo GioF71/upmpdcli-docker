351bd8748a1bd730e5923cdb3abea5ce76e6112d
Giovanni Fulco <giovanni.fulco@gmail.com>
2024-08-23 23:52:33 +0200

    get rid of PPA_NAME

--- app/install/setup-ubuntu.sh
+++ app/install/setup-ubuntu.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+PPA_NAME=upnpp1
+PPA_KEY=bd1ec68cab92bbd56698f1c507971a38c8a2ca38
+
 echo $PPA_NAME > /app/conf/ppa.txt
 
 apt-get update
@@ -11,7 +14,6 @@ mkdir -p /etc/apt/sources.list.d -p
 cp /app/install/ubuntu.${PPA_NAME}.list /etc/apt/sources.list.d/${PPA_NAME}.list
 # cat /etc/apt/sources.list.d/${PPA_NAME}.list
 sed -i "s/UBUNTU_VERSION/${IMAGE_VERSION}/g" /etc/apt/sources.list.d/${PPA_NAME}.list
-cat /etc/apt/sources.list.d/${PPA_NAME}.list
+# cat /etc/apt/sources.list.d/${PPA_NAME}.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ${PPA_KEY}
-# apt-key adv --keyserver keyserver.ubuntu.com --recv-keys bd1ec68cab92bbd56698f1c507971a38c8a2ca38
 apt-get update
--- build.sh
+++ build.sh
@@ -14,7 +14,6 @@ base_images[focal]=ubuntu:focal
 DEFAULT_BASE_IMAGE=noble
 DEFAULT_TAG=local
 DEFAULT_USE_PROXY=N
-DEFAULT_PPA=upnpp1
 DEFAULT_BUILD_MODE=full
 
 DEFAULT_CACHE_MODE=""
@@ -22,15 +21,12 @@ DEFAULT_CACHE_MODE=""
 tag=$DEFAULT_TAG
 use_proxy=$DEFAULT_USE_PROXY
 
-ppa=$DEFAULT_PPA
-
-while getopts b:t:p:e:m:c: flag
+while getopts b:t:p:m:c: flag
 do
     case "${flag}" in
         b) base_image=${OPTARG};;
         t) tag=${OPTARG};;
         p) proxy=${OPTARG};;
-        e) ppa=${OPTARG};;
         m) build_mode=${OPTARG};;
         c) cache_mode=${OPTARG};;
     esac
@@ -39,7 +35,6 @@ done
 echo "base_image: $base_image";
 echo "tag: $tag";
 echo "proxy: $proxy";
-echo "ppa: $ppa";
 echo "build_mode: $build_mode";
 echo "cache_mode: $cache_mode";
 
@@ -70,14 +65,12 @@ fi
 
 echo "Base Image: ["$select_base_image"]"
 echo "Tag: ["$tag"]"
-echo "PPA: ["$ppa"]"
 echo "Build Mode: ["$build_mode"]"
 echo "Proxy: ["$use_proxy"]"
 
 cmd_line="docker build . ${cache_mode} \
     --build-arg BASE_IMAGE=${select_base_image} \
     --build-arg USE_APT_PROXY=${use_proxy} \
-    --build-arg PPA_NAME=${ppa} \
     --build-arg BUILD_MODE=${build_mode} \
     --progress=plain \
     -t giof71/upmpdcli:$tag"
