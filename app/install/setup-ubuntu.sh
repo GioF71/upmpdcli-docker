c1411f61756ce303621781bcb30646a078c731dd
Giovanni Fulco <giovanni.fulco@gmail.com>
2024-08-23 22:26:53 +0200

    Install gnupg before adding the key

--- app/install/setup-ubuntu.sh
+++ app/install/setup-ubuntu.sh
@@ -9,6 +9,6 @@ cp /app/install/ubuntu.${PPA_NAME}.list /etc/apt/sources.list.d/${PPA_NAME}.list
 # cat /etc/apt/sources.list.d/${PPA_NAME}.list
 sed -i "s/UBUNTU_VERSION/${IMAGE_VERSION}/g" /etc/apt/sources.list.d/${PPA_NAME}.list
 # cat /etc/apt/sources.list.d/${PPA_NAME}.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ${PPA_KEY}
 apt-get update
 apt-get install -y gnupg
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ${PPA_KEY}
