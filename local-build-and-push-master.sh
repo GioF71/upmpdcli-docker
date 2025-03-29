e82071f1efaa9efa9085048758cf473f1db4d825
Giovanni Fulco <giovanni.fulco@gmail.com>
2025-03-29 11:07:10 +0100

    #477 review build process using git modules (#478)

--- local-build-and-push-master.sh
+++ local-build-and-push-master.sh
@@ -5,9 +5,8 @@ TODAY=$(date '+%Y-%m-%d')
 echo "TODAY=${TODAY}"
 
 docker buildx build . \
-    -f Dockerfile-use-lib \
     --platform linux/amd64,linux/arm64/v8,linux/arm/v7 \
-    --build-arg BRANCH_NAME=master \
+    --build-arg UPMPDCLI_SELECTOR=master \
     --build-arg BUILD_MODE=full \
     --tag giof71/upmpdcli:master \
     --tag giof71/upmpdcli:master-full \
@@ -16,9 +15,8 @@ docker buildx build . \
 
 # renderer
 docker buildx build . \
-    -f Dockerfile-use-lib \
     --platform linux/amd64,linux/arm64/v8,linux/arm/v7 \
-    --build-arg BRANCH_NAME=master \
+    --build-arg UPMPDCLI_SELECTOR=master \
     --build-arg BUILD_MODE=renderer \
     --tag giof71/upmpdcli:master-renderer \
     --tag giof71/upmpdcli:master-renderer-${TODAY} \
