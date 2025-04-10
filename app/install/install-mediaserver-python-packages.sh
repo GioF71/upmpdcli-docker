c1aaae2f781cb26a354aaaece23daa5f2f0a74e9
Giovanni Fulco <giovanni.fulco@gmail.com>
2025-04-10 08:49:35 +0200

    Update to latest master with Subsonic Plugin 0.7.7 (#486)
    - Subsonic Plugin 0.7.7
    - upgrade to beta version 0.3.10b2 of subsonic-connector (for vpn compatibility)

--- app/install/install-mediaserver-python-packages.sh
+++ app/install/install-mediaserver-python-packages.sh
@@ -5,7 +5,7 @@ set -ex
 echo "BUILD_MODE=[${BUILD_MODE}]"
 
 if [ "${BUILD_MODE}" = "full" ]; then
-    pip install --break-system-packages subsonic-connector==0.3.9
+    pip install --break-system-packages subsonic-connector==0.3.10b2
     pip install --break-system-packages tidalapi
     pip install --break-system-packages pyradios
 fi
