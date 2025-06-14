fa712ce149abf450b93c09cc951818fe0b89ee37
Giovanni Fulco <giovanni.fulco@gmail.com>
2025-06-14 18:59:24 +0200

    Add support for arm/v6

--- local-build-and-push-release.sh
+++ local-build-and-push-release.sh
@@ -5,7 +5,7 @@ TODAY=$(date '+%Y-%m-%d')
 echo "TODAY=${TODAY}"
 
 docker buildx build . \
-    --platform linux/amd64,linux/arm64/v8,linux/arm/v7 \
+    --platform linux/amd64,linux/arm64/v8,linux/arm/v7,linux/arm/v6 \
     --build-arg UPMPDCLI_SELECTOR=master \
     --build-arg BUILD_MODE=full \
     --tag giof71/upmpdcli:debian-stable-slim-full \
@@ -18,7 +18,7 @@ docker buildx build . \
 
 # renderer
 docker buildx build . \
-    --platform linux/amd64,linux/arm64/v8,linux/arm/v7 \
+    --platform linux/amd64,linux/arm64/v8,linux/arm/v7,linux/arm/v6 \
     --build-arg UPMPDCLI_SELECTOR=master \
     --build-arg BUILD_MODE=renderer \
     --tag giof71/upmpdcli:debian-stable-slim-renderer \
