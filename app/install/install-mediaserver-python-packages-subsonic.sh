c5f5254c284d8adacaf7efd6c2f8ddd2358f30f9
GioF_71 <giovanni.fulco@gmail.com>
2025-10-14 19:51:44 +0200

    Submodule master updated to Subsonic Plugin 0.8.12 (nice performance bump!) (#566)
    - Also updated subsonic-connector to 0.3.10 for submodule master

--- app/install/install-mediaserver-python-packages-subsonic.sh
+++ app/install/install-mediaserver-python-packages-subsonic.sh
@@ -6,7 +6,7 @@ if [[ "${BUILD_MODE}" = "full" ]]; then
     if [[ "${UPMPDCLI_SELECTOR}" == "edge" ]]; then
         pip install --break-system-packages subsonic-connector==0.3.10
     elif [[ "${UPMPDCLI_SELECTOR}" == "master" ]]; then
-        pip install --break-system-packages subsonic-connector==0.3.10b5
+        pip install --break-system-packages subsonic-connector==0.3.10
     else
         pip install --break-system-packages subsonic-connector==0.3.10b5
     fi
